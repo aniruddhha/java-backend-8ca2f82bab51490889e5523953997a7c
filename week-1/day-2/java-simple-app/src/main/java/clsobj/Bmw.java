f809bd914a466befce96e78fa5d63de1a72363fc
aniruddhha <aniruddha.kudalkar@gmail.com>
2022-01-04 18:00:00 +0530

    java recap : unit test cases fo inheritance

--- week-1/day-2/java-simple-app/src/main/java/clsobj/Bmw.java
+++ week-1/day-2/java-simple-app/src/main/java/clsobj/Bmw.java
@@ -1,5 +1,8 @@
 package clsobj;
 
+// bmw is a car
+// -> object of bmw should be object of car
+// -> object of car is not the object of bmw
 public class Bmw extends Car {
 
 }
--- week-1/day-2/java-simple-app/src/test/java/BmwTests.java
+++ week-1/day-2/java-simple-app/src/test/java/BmwTests.java
@@ -1,4 +1,5 @@
 import clsobj.Bmw;
+import clsobj.Car;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -22,5 +23,23 @@ public class BmwTests {
         int expectedSpeed = 10;
         int actualSpeed = bmw.speed;
         Assertions.assertEquals(expectedSpeed, actualSpeed);
+    }
+
+    @DisplayName("Test is Bmw is a Car")
+    @Test
+    void testBmwObjectCar() {
+        Bmw bmw = new Bmw();
+        boolean expected = true;
+        boolean actual = bmw instanceof Car;
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @DisplayName("Test Car is Not a BMW")
+    @Test
+    void testCarObjectNotBmw() {
+        Car car = new Car();
+        boolean expected = false;
+        boolean actual = car instanceof Bmw;
+        Assertions.assertEquals(expected, actual);
     }
 }
