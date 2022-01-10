0de062c81982b3c64cf960410c7bac6c134d9377
aniruddhha <aniruddha.kudalkar@gmail.com>
2022-01-10 11:20:20 +0530

    spring core : annotation based config

--- week-2/day-1/spring-app-annotation-based/src/main/java/com/ani/Main.java
+++ week-2/day-1/spring-app-annotation-based/src/main/java/com/ani/Main.java
@@ -1,16 +1,19 @@
 package com.ani;
 
 import com.ani.car.Car;
+import com.ani.config.AppConfig;
 import com.ani.driver.Driver;
 import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.context.support.GenericApplicationContext;
 
 import java.util.Arrays;
 
 public class Main {
 
     public static void main(String[] args) {
-        ApplicationContext context = new ClassPathXmlApplicationContext("beans.xml");
+        ApplicationContext context = new AnnotationConfigApplicationContext(AppConfig.class);
 
         Car car1 = (Car) context.getBean("car");
 
