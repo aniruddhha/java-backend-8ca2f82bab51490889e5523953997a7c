116225796756157f0375bfd94d82b5fa614c18bb
aniruddhha <aniruddha.kudalkar@gmail.com>
2022-01-10 14:51:05 +0530

    spring aop : point cuts

--- week-2/day-1/spring-app-annotation-based/src/main/java/com/ani/logging/CarLogger.java
+++ week-2/day-1/spring-app-annotation-based/src/main/java/com/ani/logging/CarLogger.java
@@ -11,11 +11,19 @@ public class CarLogger {
     //execution(public String com.ani.car.Car.speedUp())
     //execution(* com.ani.car.*.*(..))
 
+    @Pointcut("execution(public * com.ani.car.Car.speedUp(..))")
+    public void pointCutAfterExecution() { }
+
     @Before("execution(* com.ani.car.*.*(..))")
     public void logBeforeAnyMethodExecutionCarPackage(JoinPoint jp) {
         System.out.println("Before Execution I am getting printed");
         System.out.println("Executing method "+jp.getSignature());
         System.out.println("After this line you will see method output");
         System.out.println("----------------");
+    }
+
+    @After("pointCutAfterExecution()")
+    public void logAfterAnyMethods(JoinPoint jp) {
+        System.out.println("After Execution I am getting printed");
     }
 }
