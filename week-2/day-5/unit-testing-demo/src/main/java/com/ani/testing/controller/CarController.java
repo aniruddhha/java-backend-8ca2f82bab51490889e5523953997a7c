0010b042fbf28261185a55fd1295ef876b55c9ed
aniruddhha <aniruddha.kudalkar@gmail.com>
2022-01-14 10:34:01 +0530

    unit testing : rest layer

--- week-2/day-5/unit-testing-demo/src/main/java/com/ani/testing/controller/CarController.java
+++ week-2/day-5/unit-testing-demo/src/main/java/com/ani/testing/controller/CarController.java
@@ -1,11 +1,9 @@
 package com.ani.testing.controller;
 
 import com.ani.testing.domain.Car;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.sql.Date;
 import java.time.LocalDate;
@@ -22,5 +20,10 @@ public class CarController {
         car.setModel("abc");
         car.setMfg(Date.valueOf(LocalDate.now()));
         return ResponseEntity.ok(car);
+    }
+
+    @PostMapping
+    public ResponseEntity<Car> saveCar(@RequestBody Car car) {
+        return new ResponseEntity<>(car, HttpStatus.CREATED);
     }
 }
--- week-2/day-5/unit-testing-demo/src/test/java/com/ani/testing/controller/CarControllerTests.java
+++ week-2/day-5/unit-testing-demo/src/test/java/com/ani/testing/controller/CarControllerTests.java
@@ -2,29 +2,67 @@ package com.ani.testing.controller;
 
 import com.ani.testing.domain.Car;
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.boot.web.server.LocalServerPort;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+
+import java.sql.Date;
+import java.time.LocalDate;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 public class CarControllerTests {
 
     @Autowired
-    private TestRestTemplate template;
+    private TestRestTemplate template; // it is wrapper over http client
 
     @LocalServerPort
     private int port;
 
+    @DisplayName("GET - Car - Checking Object is non null")
     @Test
     public void testGetMethod() {
-        var builder = new StringBuilder();
-        Car car = template.getForObject(
-                "http://localhost:8080/car",
+
+        String url = "http://" + "localhost" +":" + port +"/car";
+
+        Car car = template.getForObject( // GET method
+                url,
                 Car.class
         );
 
         Assertions.assertNotNull(car);
+    }
+
+    @DisplayName("GET - Car - Checking Status Code")
+    @Test
+    public void testGetStatusCode() {
+        String url = "http://" + "localhost" +":" + port +"/car";
+        ResponseEntity<Car> entity = template.getForEntity(url, Car.class);
+        Assertions.assertEquals(
+                HttpStatus.OK,
+                entity.getStatusCode()
+        );
+    }
+
+    @DisplayName("POST - Car - Saving Car Object")
+    @Test
+    public void testPostMethod() {
+        String url = "http://" + "localhost" +":" + port +"/car";
+        var car = new Car();
+        car.setCost(56.23);
+        car.setId(10L);
+        car.setModel("abc");
+        car.setMfg(Date.valueOf(LocalDate.now()));
+        var re = template.postForEntity(
+                url,
+                car,
+                Car.class // type information of Car -> this is skeleton of the car
+        );
+        Assertions.assertEquals(HttpStatus.CREATED, re.getStatusCode());
     }
 }
