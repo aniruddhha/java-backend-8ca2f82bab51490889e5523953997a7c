11dce160fe60226708a5fe384076c35d0ed7fd47
aniruddhha <aniruddha.kudalkar@gmail.com>
2022-01-17 12:53:51 +0530

    microservice : api gateway

---
+++ week-3/microservice-basics/gateway-app/src/main/java/com/ani/gateway/GatewayAppApplication.java
@@ -0,0 +1,13 @@
+package com.ani.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GatewayAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GatewayAppApplication.class, args);
+	}
+
+}
