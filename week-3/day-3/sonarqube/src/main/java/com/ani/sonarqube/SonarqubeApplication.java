b9e23dfbd0022a39ed66e2997813949f7624c206
aniruddhha <aniruddha.kudalkar@gmail.com>
2022-01-20 09:48:37 +0530

    ani practice

---
+++ week-3/day-3/sonarqube/src/main/java/com/ani/sonarqube/SonarqubeApplication.java
@@ -0,0 +1,13 @@
+package com.ani.sonarqube;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SonarqubeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SonarqubeApplication.class, args);
+	}
+
+}
---
+++ week-3/day-3/sonarqube/src/test/java/com/ani/sonarqube/SonarqubeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ani.sonarqube;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SonarqubeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
